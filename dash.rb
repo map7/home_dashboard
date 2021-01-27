c829d8ff5cbfa20066958a4158d04e1730a4734a
Michael Pope <map7777@gmail.com>
2021-01-27 22:48:42 +1100

    Add real time

--- dash.rb
+++ dash.rb
@@ -1,1 +1,17 @@
+Shoes.app width: 720, height: 480 do
+  background "#000"
+  @clock = para "00:00"
+  @clock.style(:stroke => red, :font => "130")
+
+  @temperature = para "00C "
+  @temperature.style(:stroke => red, :font => "40")
+
+  @humidity = para "00%"
+  @humidity.style(:stroke => red, :font => "40")
+
+  every 1 do 
+    @clock.text = Time.now.strftime("%H:%M")
+    
+  end
+ end
  
