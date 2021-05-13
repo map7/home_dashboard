23b2e452eeb8c25977516103dab03d6f6eff8ced
Michael Pope <map7777@gmail.com>
2021-05-13 21:14:30 +1000

    change presentation of temp

--- dash.rb
+++ dash.rb
@@ -1,6 +1,6 @@
 require 'net/http'
 require 'json'
-require 'byebug'
+#require 'byebug'
 
 url = 'https://api.thingspeak.com/channels/1097373/feeds/last.json'
 uri = URI(url)
@@ -22,7 +22,7 @@ Shoes.app fullscreen: true, width: 800, height: 400 do
     # Update Temperature & Humidity
     response = Net::HTTP.get(uri)
     response = JSON.parse(response)
-    @temperature.text = response["field1"] + "C " 
+    @temperature.text = response["field1"].to_i.to_s.rjust(2,"0") + "C " 
     @humidity.text = response["field2"] + "%"
   end
  end
